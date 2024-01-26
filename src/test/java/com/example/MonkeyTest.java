1ac81ab7def8bd5021418f20d4dd904d7fdb1c60
JC Mann <john.mann@astrazeneca.com>
2024-01-26 10:29:30 -0500

    Added an assertion to verify that a RedisConnectionException is thrown.

--- src/test/java/com/example/MonkeyTest.java
+++ src/test/java/com/example/MonkeyTest.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import io.lettuce.core.RedisConnectionException;
 import io.micronaut.context.ApplicationContext;
 import io.micronaut.context.exceptions.BeanInstantiationException;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
@@ -7,8 +8,7 @@ import jakarta.inject.Inject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @MicronautTest
 class MonkeyTest {
@@ -29,6 +29,7 @@ class MonkeyTest {
             monkey = applicationContext.getBean(Monkey.class);
         } catch (BeanInstantiationException e) {
             assertNull(monkey);
+            assertInstanceOf(RedisConnectionException.class, e.getCause(), "Root cause must be of type RedisConnectionException");
             int callCount = redisErrorHandler.getCallCount();
             assertTrue(callCount > initialCallCount,
                        "RedisErrorHandler call count (" + callCount + ") must be greater than the initial RedisErrorHandler call count (" + initialCallCount + ").");
